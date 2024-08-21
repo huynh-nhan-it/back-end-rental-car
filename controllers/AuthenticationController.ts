a2785d937200a6b09e1b3ebcd80088e14c4aca48
huynh-nhan-it <128890299+huynh-nhan-it@users.noreply.github.com>
2024-08-21 21:39:42 +0700

    Update AuthenticationController.ts

--- controllers/AuthenticationController.ts
+++ controllers/AuthenticationController.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User";
-import Token from "../models/Token";
+const Token = require('../models/Token');
 import * as bcrypt from "bcrypt";
 import helpers from "../untils/helpers";
 import { OAuth2Client } from "google-auth-library";
