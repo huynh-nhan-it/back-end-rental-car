cbc91e499b6a55fa4709dc5f1629173086e785d5
huynh-nhan-it <128890299+huynh-nhan-it@users.noreply.github.com>
2024-08-25 19:48:51 +0700

    Update Token.ts

--- models/Token.ts
+++ models/Token.ts
@@ -3,7 +3,7 @@ import { mongoose, Schema } from "../untils/utilities_import";
 const TokenSchema: Schema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   token: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: '10d' }
+  createdAt: { type: Date, default: Date.now, expires: '10d' } // Token hết hạn sau 10 ngày
 });
 
 export default mongoose.model('Token', TokenSchema);
